6f122af0bf298e59ac4fa8f960d6a4f0174ac7f1
Pranshu Patel <pranshu.patel@simformsolutions.com>
2025-05-23 15:51:07 +0530

    Add: configure react router dom and create main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
+
+import NotFound from './components/NotFound';
+import Login from './features/Authentication/pages/Login';
+import MainLayout from './layouts/MainLayout';
+
 function App() {
-  return <></>;
+  return (
+    <div className="">
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<h1>Home</h1>} />
+          <Route path="/login" element={<Login />} />
+        </Route>
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
