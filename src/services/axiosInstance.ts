dba0652a2894c00f7850c18e5fafea7106b8ac8b
Pranshu Patel <pranshu.patel@simformsolutions.com>
2025-06-02 13:58:06 +0530

    Update: update the api URL with .env variable

--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const strapiURL = import.meta.env.VITE_STRAPI_URL;
+
 const instance = axios.create({
-  baseURL: 'http://localhost:1337/api/',
+  baseURL: `${strapiURL}/api/`,
 });
 
 export { instance };
