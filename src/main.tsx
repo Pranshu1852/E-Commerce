365fbe56845be709135bb2413fd91cceffdc6a1d
Pranshu Patel <pranshu.patel@simformsolutions.com>
2025-06-02 13:47:55 +0530

    Add: implement error boundary to handle fallback

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,13 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import { store } from './store/store.ts';
 import './lib/i18n.ts';
+import { NavigationBridgeComponent } from './components/NavigationBridge.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
+        <NavigationBridgeComponent />
         <App />
       </BrowserRouter>
     </Provider>
