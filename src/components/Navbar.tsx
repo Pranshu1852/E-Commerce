3b764474e40818be8e0b97b8171fa8a2245e2f91
Pranshu Patel <pranshu.patel@simformsolutions.com>
2025-06-04 10:48:10 +0530

    Add: create profileDropdown menu and logout functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,12 @@
 import SearchIcon from '@mui/icons-material/Search';
-import type { ChangeEvent } from 'react';
+import { type ChangeEvent } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link, useSearchParams } from 'react-router-dom';
 
+import iconLogo from '../assets/logo.svg';
+
 import LanguageSelector from './LanguageSelector';
+import ProfileDropDown from './ProfileDropDown';
 
 function Navbar() {
   const { t } = useTranslation();
@@ -24,8 +27,12 @@ function Navbar() {
 
   return (
     <div className='bg-cyan-500 shadow-md flex flex-row flex-wrap items-center justify-between gap-5 py-5 px-10'>
-      <Link to='/' className='text-3xl text-white font-bold'>
-        {t('logo')}
+      <Link
+        to='/'
+        className='flex flex-row items-center gap-2 text-3xl text-white font-bold'
+      >
+        <img className='w-9' src={iconLogo} alt='logo' />
+        <h1>{t('logo')}</h1>
       </Link>
       <div className='relative flex flex-row basis-[363px] items-center bg-white flex-1 md:max-w-[600px] rounded-full focus:ring-2 order-1 md:order-none'>
         <SearchIcon
@@ -37,12 +44,14 @@ function Navbar() {
         />
         <input
           onChange={handleChange}
+          value={searchParams.get('search') ?? ''}
           className='h-full w-full py-3 rounded-full pl-10 outline-none focus:ring-2 focus:ring-cyan-800'
           type='text'
           placeholder='Search Products...'
         />
       </div>
       <div className='flex flex-row items-center gap-5 font-semibold text-white text-xl'>
+        <ProfileDropDown />
         <LanguageSelector />
       </div>
     </div>
--- src/utils/storageHandler.ts
+++ src/utils/storageHandler.ts
@@ -1,17 +1,34 @@
 interface StorageHandler {
   getStorage: <T>(key: string) => T;
   setStorage: <T>(key: string, val: T) => void;
+  removeStorage: (key: string) => void;
 }
 
 const storageHandler: StorageHandler = {
   getStorage(key: string) {
-    return localStorage.getItem(key)
-      ? JSON.parse(localStorage.getItem(key)!)
-      : null;
+    try {
+      return localStorage.getItem(key)
+        ? JSON.parse(localStorage.getItem(key)!)
+        : null;
+    } catch (error) {
+      console.error(error);
+    }
   },
 
   setStorage(key: string, val) {
-    localStorage.setItem(key, JSON.stringify(val));
+    try {
+      localStorage.setItem(key, JSON.stringify(val));
+    } catch (error) {
+      console.error(error);
+    }
+  },
+
+  removeStorage(key: string) {
+    try {
+      localStorage.removeItem(key);
+    } catch (error) {
+      console.error(error);
+    }
   },
 };
 
